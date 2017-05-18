dccc461139d2d711b019f90ff804e6f86c1bb9df
DamnedScholar <stickfigureonfire@gmail.com>
2017-05-18 16:42:08 -0500

    There's a character sheet now.

--- commands/command.py
+++ commands/command.py
@@ -6,8 +6,10 @@ Commands describe the input the player can do to the game.
 """
 
 from evennia import Command as BaseCommand
-# from evennia import default_cmds
-
+from evennia import default_cmds
+from evennia.utils import evtable
+import math
+import string
 
 class Command(BaseCommand):
     """
@@ -31,155 +33,199 @@ class Command(BaseCommand):
     """
     pass
 
-# -------------------------------------------------------------
-#
-# The default commands inherit from
-#
-#   evennia.commands.default.muxcommand.MuxCommand.
-#
-# If you want to make sweeping changes to default commands you can
-# uncomment this copy of the MuxCommand parent and add
-#
-#   COMMAND_DEFAULT_CLASS = "commands.command.MuxCommand"
-#
-# to your settings file. Be warned that the default commands expect
-# the functionality implemented in the parse() method, so be
-# careful with what you change.
-#
-# -------------------------------------------------------------
+class CmdSheet(default_cmds.MuxCommand):
+    """
+    Displays your sheet, or initializes a blank sheet (for restarting chargen, hard-resetting in the case of bugs, or giving sheets to objects that don't have them).
 
-# from evennia.utils import utils
-#
-#
-# class MuxCommand(Command):
-#     """
-#     This sets up the basis for a MUX command. The idea
-#     is that most other Mux-related commands should just
-#     inherit from this and don't have to implement much
-#     parsing of their own unless they do something particularly
-#     advanced.
-#
-#     Note that the class's __doc__ string (this text) is
-#     used by Evennia to create the automatic help entry for
-#     the command, so make sure to document consistently here.
-#     """
-#     def has_perm(self, srcobj):
-#         """
-#         This is called by the cmdhandler to determine
-#         if srcobj is allowed to execute this command.
-#         We just show it here for completeness - we
-#         are satisfied using the default check in Command.
-#         """
-#         return super(MuxCommand, self).has_perm(srcobj)
-#
-#     def at_pre_cmd(self):
-#         """
-#         This hook is called before self.parse() on all commands
-#         """
-#         pass
-#
-#     def at_post_cmd(self):
-#         """
-#         This hook is called after the command has finished executing
-#         (after self.func()).
-#         """
-#         pass
-#
-#     def parse(self):
-#         """
-#         This method is called by the cmdhandler once the command name
-#         has been identified. It creates a new set of member variables
-#         that can be later accessed from self.func() (see below)
-#
-#         The following variables are available for our use when entering this
-#         method (from the command definition, and assigned on the fly by the
-#         cmdhandler):
-#            self.key - the name of this command ('look')
-#            self.aliases - the aliases of this cmd ('l')
-#            self.permissions - permission string for this command
-#            self.help_category - overall category of command
-#
-#            self.caller - the object calling this command
-#            self.cmdstring - the actual command name used to call this
-#                             (this allows you to know which alias was used,
-#                              for example)
-#            self.args - the raw input; everything following self.cmdstring.
-#            self.cmdset - the cmdset from which this command was picked. Not
-#                          often used (useful for commands like 'help' or to
-#                          list all available commands etc)
-#            self.obj - the object on which this command was defined. It is often
-#                          the same as self.caller.
-#
-#         A MUX command has the following possible syntax:
-#
-#           name[ with several words][/switch[/switch..]] arg1[,arg2,...] [[=|,] arg[,..]]
-#
-#         The 'name[ with several words]' part is already dealt with by the
-#         cmdhandler at this point, and stored in self.cmdname (we don't use
-#         it here). The rest of the command is stored in self.args, which can
-#         start with the switch indicator /.
-#
-#         This parser breaks self.args into its constituents and stores them in the
-#         following variables:
-#           self.switches = [list of /switches (without the /)]
-#           self.raw = This is the raw argument input, including switches
-#           self.args = This is re-defined to be everything *except* the switches
-#           self.lhs = Everything to the left of = (lhs:'left-hand side'). If
-#                      no = is found, this is identical to self.args.
-#           self.rhs: Everything to the right of = (rhs:'right-hand side').
-#                     If no '=' is found, this is None.
-#           self.lhslist - [self.lhs split into a list by comma]
-#           self.rhslist - [list of self.rhs split into a list by comma]
-#           self.arglist = [list of space-separated args (stripped, including '=' if it exists)]
-#
-#           All args and list members are stripped of excess whitespace around the
-#           strings, but case is preserved.
-#         """
-#         raw = self.args
-#         args = raw.strip()
-#
-#         # split out switches
-#         switches = []
-#         if args and len(args) > 1 and args[0] == "/":
-#             # we have a switch, or a set of switches. These end with a space.
-#             switches = args[1:].split(None, 1)
-#             if len(switches) > 1:
-#                 switches, args = switches
-#                 switches = switches.split('/')
-#             else:
-#                 args = ""
-#                 switches = switches[0].split('/')
-#         arglist = [arg.strip() for arg in args.split()]
-#
-#         # check for arg1, arg2, ... = argA, argB, ... constructs
-#         lhs, rhs = args, None
-#         lhslist, rhslist = [arg.strip() for arg in args.split(',')], []
-#         if args and '=' in args:
-#             lhs, rhs = [arg.strip() for arg in args.split('=', 1)]
-#             lhslist = [arg.strip() for arg in lhs.split(',')]
-#             rhslist = [arg.strip() for arg in rhs.split(',')]
-#
-#         # save to object properties:
-#         self.raw = raw
-#         self.switches = switches
-#         self.args = args.strip()
-#         self.arglist = arglist
-#         self.lhs = lhs
-#         self.lhslist = lhslist
-#         self.rhs = rhs
-#         self.rhslist = rhslist
-#
-#         # if the class has the player_caller property set on itself, we make
-#         # sure that self.caller is always the player if possible. We also create
-#         # a special property "character" for the puppeted object, if any. This
-#         # is convenient for commands defined on the Player only.
-#         if hasattr(self, "player_caller") and self.player_caller:
-#             if utils.inherits_from(self.caller, "evennia.objects.objects.DefaultObject"):
-#                 # caller is an Object/Character
-#                 self.character = self.caller
-#                 self.caller = self.caller.player
-#             elif utils.inherits_from(self.caller, "evennia.players.players.DefaultPlayer"):
-#                 # caller was already a Player
-#                 self.character = self.caller.get_puppet(self.session)
-#             else:
-#                 self.character = None
+    Usage:
+        sheet
+        sheet <target>
+        sheet/blank
+        sheet/blank <target>
+        sheet/prove
+        sheet/prove <target>
+        sheet/show
+        sheet/show <target>
+    """
+
+    key = "sheet"
+    help_category = "Shadowrun 5e"
+
+    def func(self):
+        "Active function."
+        caller = self.caller
+        if self.args:
+            target = self.args
+        elif self.args:
+            return caller.msg("I can't find a player by that name.")
+        else:
+            target = caller
+
+        if not self.switches:
+            # Display the character sheet. If the object doesn't have a character sheet, display an error message.
+            if not self.args:
+                # Display the caller's sheet.
+                vitals = evtable.EvTable(width=120, align="l")
+                vitals.add_row("Full Name:",target.db.fullname,
+                               "Birthdate:",target.db.birthdate)
+                vitals.add_row("Metatype:",target.db.metatype,
+                               "Ethnicity:",target.db.ethnicity)
+                vitals.add_row("Height:",target.db.height,
+                               "Weight:",target.db.weight)
+                rep = evtable.EvTable(width=120, align="l")
+                rep.add_row("Street Cred:",target.db.street_cred,
+                            "Notoriety:",target.db.notoriety,
+                            "Public Awareness:",target.db.public_awareness)
+                karma = evtable.EvTable(width=120, align="l")
+                karma.add_row("Karma:",target.db.karma["current"],
+                               "Total:",target.db.karma["total"])
+                caller.msg(vitals)
+                caller.msg(rep)
+                caller.msg(karma)
+
+                attributes = evtable.EvTable(width=120, align="l")
+                attributes.add_row("BOD:",target.get_bod(),
+                                   "AGI:",target.get_agi(),
+                                   "REA:",target.get_rea(),
+                                   "STR:",target.get_str())
+                attributes.add_row("WIL:",target.get_bod(),
+                                   "LOG:",target.get_agi(),
+                                   "INT:",target.get_rea(),
+                                   "CHA:",target.get_str())
+                attributes.add_row("Essence:",target.get_ess(),
+                                   "Edge:",target.get_edg(),
+                                   "Magic:",target.get_mag(),
+                                   "Resonance:",target.get_res())
+                caller.msg(attributes)
+
+                def cond_count(i, t):
+                    if i + 3 <= t:
+                        i += 3
+                        return "[ ][ ][ ]"
+                    elif i + 2 == t:
+                        i += 2
+                        return "[ ][ ]   "
+                    elif i + 1 == t:
+                        i += 1
+                        return "[ ]      "
+                    else:
+                        return "         "
+
+                phys_mod = target.get_phys_mod()
+                stun_mod = target.get_stun_mod()
+                derived = evtable.EvTable(width=120, align="l", border = "none")
+                derived.add_row("Initiative:",target.get_init(),
+                                "Physical Mod:",
+                                "Stun Mod:")
+                derived.add_row("Astral Initiative:",target.get_astral_init(),
+                                cond_count(0, phys_mod),
+                                cond_count(0, stun_mod))
+                derived.add_row("Matrix Initiative:",target.get_matrix_init(),
+                                cond_count(3, phys_mod),
+                                cond_count(3, stun_mod))
+                derived.add_row("Composure:",target.get_composure(),
+                                cond_count(6, phys_mod),
+                                cond_count(6, stun_mod))
+                derived.add_row("Judge Intentions:",target.get_judge(),
+                                cond_count(9, phys_mod),
+                                cond_count(9, stun_mod))
+                derived.add_row("Memory:",target.get_memory(),
+                                cond_count(12, phys_mod))
+                derived.add_row("Lift/Carry:",target.get_lift_carry(),
+                                cond_count(15, phys_mod))
+                derived.add_row("Movement:",target.get_movement())
+                caller.msg(derived)
+
+                skills = evtable.EvTable("Skills", width=120, align="l",
+                                         border = "header")
+                # TODO: Add active skill groups.
+                active_skills = ""
+                skill_list = target.db.active_skills.items() + target.db.active_skill_groups.items()
+                def getkey(tuple):
+                    return tuple[0]
+
+                skill_list.sort(key=getkey)
+                for i in range(0, len(target.db.active_skills)):
+                    skill = skill_list[i]
+                    skill_string = string.capwords(skill[0])
+                    if skill[0] in target.db.active_specializations:
+                        skill_string += " (" + string.capwords(target.db.active_specializations[skill[0]]) + " +2)"
+                    active_skills += skill_string + " " + str(skill[1])
+
+                    if i < len(target.db.active_skills) - 1:
+                        active_skills += ", "
+
+                skills.add_row("Active", active_skills)
+
+                knowledge_skills = ""
+                skill_list = target.db.knowledge_skills.items()
+                skill_list.sort(key=getkey)
+
+                for i in range(0, len(target.db.knowledge_skills)):
+                    skill = skill_list[i]
+                    skill_string = string.capwords(skill[0])
+                    if skill[0] in target.db.knowledge_specializations:
+                        skill_string += " (" + string.capwords(target.db.knowledge_specializations[skill[0]]) + " +2)"
+                    knowledge_skills += skill_string + " " + str(skill[1])
+
+                    if i < len(target.db.knowledge_skills) - 1:
+                        knowledge_skills += ", "
+
+                skills.add_row("Knowledge", knowledge_skills)
+
+                language_skills = ""
+                skill_list = target.db.language_skills.items()
+                skill_list.sort(key=getkey)
+                
+                for i in range(0, len(target.db.language_skills)):
+                    skill = skill_list[i]
+                    skill_string = string.capwords(skill[0])
+                    if skill[0] in target.db.language_specializations:
+                        skill_string += " (" + string.capwords(target.db.language_specializations[skill[0]]) + " +2)"
+                    language_skills += skill_string + " " + str(skill[1])
+
+                    if i < len(target.db.language_skills) - 1:
+                        language_skills += ", "
+
+                skills.add_row("Language", language_skills)
+                caller.msg(skills)
+            else:
+                # Look for an object that matches self.args and display its sheet if the caller has the authority to see it.
+                pass
+        elif "blank" in self.switches:
+            # Initialize a blank sheet. If the object has a sheet, we might want to prompt for authorization to erase it. If the object's sheet is locked, this command will return an error.
+            if not target.sheet_locked == true:
+                target.db.sheet_locked = "False"
+                target.db.fullname = "Empty"
+                target.db.approved = "No"
+                target.db.birthdate = "1/1/1970"
+                target.db.metatype = "Human"
+                target.db.ethnicity = ""
+                target.db.height = ""
+                target.db.weight = ""
+                target.db.street_cred = ""
+                target.db.notoriety = ""
+                target.db.public_awareness = ""
+                target.db.karma = {'current': 0, 'total': 0}
+                target.db.attributes = {
+                    'body': 1, 'agility': 1, 'reaction': 1, 'strength': 1,
+                    'willpower': 1, 'logic': 1, 'intuition': 1, 'charisma': 1,
+                    'essence': 6, 'edge': 1, 'magic': 0, 'resonance': 0
+                }
+                target.db.active_skills = {}
+                target.db.active_specializations = {}
+                target.db.active_skill_groups = {}
+                target.db.knowledge_skills = {}
+                target.db.knowledge_specializations = {}
+                target.db.language_skills = {}
+                target.db.language_specializations = {}
+                # Don't store the Karma value of qualities. Store the level and calculate the Karma points based on the lookup table.
+                target.db.qualities_positive = {}
+                target.db.qualities_negative={}
+            else:
+                return caller.msg("That sheet is locked. A member of staff will have to unlock it.")
+        elif "prove" in self.switches or "show" in self.switches:
+            # Show your sheet to someone else or the room.
+            caller.msg("Prove command coming soon.")
+        else:
+            caller.msg("I don't know what switch that is.")
---
+++ server/conf/settings.py
@@ -0,0 +1,75 @@
+"""
+Evennia settings file.
+
+The available options are found in the default settings file found
+here:
+
+D:\github\evennia\evennia\settings_default.py
+
+Remember:
+
+Don't copy more from the default file than you actually intend to
+change; this will make sure that you don't overload upstream updates
+unnecessarily.
+
+When changing a setting requiring a file system path (like
+path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
+your game folder and the Evennia library folders respectively. Python
+paths (path.to.module) should be given relative to the game's root
+folder (typeclasses.foo) whereas paths within the Evennia library
+needs to be given explicitly (evennia.foo).
+
+"""
+
+# Use the defaults from Evennia unless explicitly overridden
+from evennia.settings_default import *
+
+######################################################################
+# Evennia base server config
+######################################################################
+
+# This is the name of your game. Make it catchy!
+SERVERNAME = "Testbox"
+# A list of ports the Evennia telnet server listens on Can be one or many.
+TELNET_PORTS = [4010]
+# The webserver sits behind a Portal proxy. This is a list
+# of tuples (proxyport,serverport) used. The proxyports are what
+# the Portal proxy presents to the world. The serverports are
+# the internal ports the proxy uses to forward data to the Server-side
+# webserver (these should not be publicly open)
+WEBSERVER_PORTS = [(8010, 5011)]
+# Ports to use for SSH
+SSH_PORTS = [8032]
+# Ports to use for SSL
+SSL_PORTS = [4011]
+# The game server opens an AMP port so that the portal can
+# communicate with it. This is an internal functionality of Evennia, usually
+# operating between two processes on the same machine. You usually don't need to
+# change this unless you cannot use the default AMP port/host for
+# whatever reason.
+AMP_HOST = 'localhost'
+AMP_PORT = 5010
+AMP_INTERFACE = '127.0.0.1'
+# Server-side websocket port to open for the webclient.
+WEBSOCKET_CLIENT_PORT = 8001
+# This is a security setting protecting against host poisoning
+# attacks.  It defaults to allowing all. In production, make
+# sure to change this to your actual host addresses/IPs.
+ALLOWED_HOSTS = ["*"]
+# How long time (in seconds) a user may idle before being logged
+# out. This can be set as big as desired. A user may avoid being
+# thrown off by sending the empty system command 'idle' to the server
+# at regular intervals. Set <=0 to deactivate idle timeout completely.
+# IDLE_TIMEOUT = 259200 would make three days.
+IDLE_TIMEOUT = -1
+
+######################################################################
+# Django web features
+######################################################################
+
+
+# The secret key is randomly seeded upon creation. It is used to sign
+# Django's cookies. Do not share this with anyone. Changing it will
+# log out all active web browsing sessions. Game web client sessions
+# may survive.
+SECRET_KEY = 'Y*K]@~XvL?Q-CPZOHjnx(gFR=!#mJ;8l/W<Uu>9:'
