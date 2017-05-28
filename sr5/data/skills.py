9ec28c1da8097567e9b2573f176d0278d2868e0c
DamnedScholar <stickfigureonfire@gmail.com>
2017-05-28 01:42:41 -0500

    Skills!

---
+++ sr5/data/skills.py
@@ -0,0 +1,18 @@
+class Skills():
+"""
+This class has been automatically generated based on a Google sheet.
+"""
+	archery = {
+		"attribute": "agility", "default": "yes",
+		"group": "none", "category": "combat",
+		"description": "Archery is used to fire string-loaded projectile weapons. An archer is familiar with many different styles of bow and the multitude of arrows that can be used to maximum effect.",
+		"specs": "Bow, Crossbow, Non-Standard Ammunition, Slingshot",
+		"source": "SR p. 130"
+	}
+	automatics = {
+		"attribute": "agility", "default": "yes",
+		"group": "firearms", "category": "combat",
+		"description": "The Automatics skill covers a specific subset of firearms larger than handheld pistols but smaller than rifles. This category includes submachine guns and other fully automatic carbines.",
+		"specs": "Assault Rifles, Cyber-Implant, Machine Pistols, Submachine Guns",
+		"source": "SR p. 130"
+	}
