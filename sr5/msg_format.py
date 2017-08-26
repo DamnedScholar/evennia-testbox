f6b9a18e4ad8c3313d3c6db456c884595b8ba5b0
DamnedScholar <stickfigureonfire@gmail.com>
2017-08-25 20:35:11 -0500

    Formatting upgrades.

--- sr5/msg_format.py
+++ sr5/msg_format.py
@@ -1,8 +1,15 @@
 class mf:
     @classmethod
     def header(self, text=""):
+        # if text:
+        #     text = ">|n |h{}|n |R<".format(text)
+        # return "    |R{:~^71}|n    ".format(text)
         if text:
-            text = ">|n |h{}|n |R<".format(text)
-        return "    |R{:~^71}|n    ".format(text)
+            text = "$pad(>|n |h{}|n |r<, 70, c,~)".format(text)
+        else:
+            text = "~" * 70
+
+        text = "$pad({}, 78, c, )".format(text)
+        return text
 
     tag = "|Rsr5 > |n"
